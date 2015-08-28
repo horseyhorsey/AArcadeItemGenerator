2697728c0a63dd7ed69662e86ce23a22f775f260
dave <dub.cyclone@gmail.com>
2015-08-28 21:24:40 +0100

    Initial commit

---
+++ AAItemGen/AAItems.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Xml;
+using System.IO;
+
+namespace AAItemGen
+{
+    class AAItems
+    {
+        public string ItemFileName { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string FileLocation { get; set; }
+        public string App { get; set; }
+        public string MarqueeLocation { get; set; }
+        public string ScreensLocation { get; set; }
+        public string LastModel { get; set; }
+
+        public List<AAItems> gamesList = new List<AAItems>();
+       
+        public void createJsonText(List<AAItems> list)
+        {
+            string endPath = string.Empty;
+
+            foreach (AAItems item in list)
+            {
+                JObject rss =
+
+                new JObject(
+                new JProperty("title", item.Title),
+                new JProperty("type", item.Type),
+                new JProperty("filelocation", item.FileLocation),
+                new JProperty("app", item.App),
+                new JProperty("marqueeslocation", item.MarqueeLocation),
+                new JProperty("screenslocation", item.ScreensLocation),
+                new JProperty("lastmodel", item.LastModel));
+
+                string jsonObjectString = rss.ToString();
+                jsonObjectString = jsonObjectString.Insert(0, "item\n");
+                jsonObjectString = jsonObjectString.Replace(",", string.Empty);
+                jsonObjectString = jsonObjectString.Replace(@""":", @"""");
+                jsonObjectString = jsonObjectString.Replace("null", @"""""");
+                jsonObjectString = jsonObjectString.Replace(@"\\", @"\");
+
+                createItemFile(item.ItemFileName, jsonObjectString);
+            }
+
+             //return jsonObjectString;
+        }
+
+        public void createItemFile(string fileName,string textToWrite)
+        {
+            // Check if file already exists. If yes, delete it. 
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+
+            if (!Directory.Exists(fileName))
+            {
+                string folderName = System.IO.Path.GetDirectoryName(fileName);
+                Directory.CreateDirectory(folderName);
+            }
+
+            // Create a new file 
+            using (FileStream fs = File.Create(fileName))
+            {
+                // Add some text to file
+                Byte[] title = new UTF8Encoding(true).GetBytes(textToWrite);
+                fs.Write(title, 0, title.Length);
+            }
+        }
+
+        public void generateItemsFromHS(string xmlPath, bool clonesOn,string RomFolder, string type,string ext,
+                                        string app,string modelPath, string sysName,string AaPath, string screens="",string marquees="")
+        {
+
+            //I:\Roms\PC Games\SteamLibrary\SteamApps\common\Anarchy Arcade\aarcade\library\itemType
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load(xmlPath);
+
+            string name = string.Empty;
+            string cloneOf = string.Empty;
+
+            List<AAItems> gamesList = new List<AAItems>();
+
+            foreach (XmlNode node in xdoc.SelectNodes("menu/game"))
+            {
+                name = node.SelectSingleNode("@name").InnerText;
+                cloneOf = string.Empty;
+
+                if (node.SelectSingleNode("cloneof") != null)
+                    cloneOf = node.SelectSingleNode("cloneof").InnerText;
+
+                string location = RomFolder + "\\" + name + "." + ext;
+                string joinedName = name.Replace(" ", "");
+                string endPath = System.IO.Path.Combine(AaPath, @"aarcade\library", type, joinedName + sysName + ".itm");
+
+                if (!clonesOn)
+                {
+                    if (cloneOf == string.Empty)
+                        gamesList.Add(new AAItems { Title = name, Type = type, FileLocation = location, App = app, LastModel = modelPath, ScreensLocation = screens + "\\" + name + ".png", MarqueeLocation = marquees + "\\" + name + ".png", ItemFileName = endPath });
+                }
+                else
+                    gamesList.Add(new AAItems { Title = name, Type = type, FileLocation = location, App = app, LastModel = modelPath, ScreensLocation = screens + "\\" + name + ".png", MarqueeLocation = marquees + "\\" + name + ".png", ItemFileName = endPath });
+
+           
+
+            }
+
+            createJsonText(gamesList);
+
+        }
+
+        public void generateItemsFromFolder(string RomFolder, string type,string ext,
+                                        string app,string modelPath, string sysName,string AaPath, string screens="",string marquees="")
+
+        {
+
+
+            List<AAItems> gamesList = new List<AAItems>();
+
+
+
+            foreach (string item in Directory.GetFiles(RomFolder + "\\", "*." + ext))
+            {
+
+                string shortRomName = System.IO.Path.GetFileNameWithoutExtension(item);
+
+                string joinedName = shortRomName.Replace(" ", "");
+              
+               string endPath = System.IO.Path.Combine(AaPath, @"aarcade\library", type, joinedName + type + ".itm");
+
+               gamesList.Add(new AAItems { Title = shortRomName, Type = type, FileLocation = item, App = app, LastModel = modelPath, ScreensLocation = screens + "\\" + shortRomName + ".png", MarqueeLocation = marquees + "\\" + shortRomName + ".png", ItemFileName = endPath });
+
+            }
+
+            createJsonText(gamesList);
+        }
+
+    }
+}
---
+++ AAItemGen/MainWindow.xaml.cs
@@ -0,0 +1,182 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Winforms = System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AAItemGen
+{
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        AAItems itemGenerator = new AAItems();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+           _TextboxHSPath.Text = Properties.Settings.Default.hsPath;
+           _TextboxAAPath.Text = Properties.Settings.Default.aaPath;
+
+            //AAItems itemGenerator = new AAItems();
+            //AAItems gameItem = new AAItems
+            //{
+            //    Title = "robocop",
+            //    Type = "MAME",
+            //    FileLocation = @"I:\HyperSpin\Media\Amstrad CPC\Video\3D Pool (Europe).png",
+            //    App = "RocketLaunch"
+            //};
+            //itemGenerator.gamesList  = new List<AAItems>();
+            //gameItem.gamesList.Add(gameItem);
+
+            //textBlock.Text = itemGenerator.createJsonText(gameItem.gamesList);
+
+            buildSystemsFromDatabase();
+        }
+
+        private void buildSystemsFromDatabase()
+        {
+            if (_TextboxHSPath.Text != string.Empty)
+            {
+                string menuXml = _TextboxHSPath.Text + "\\Databases\\Main Menu\\Main Menu.xml";
+                if (File.Exists(menuXml))
+                {
+                    Menu menu = new Menu();
+                    List<Menu> menuList = menu.getMainMenuItemsFromXml(menuXml);
+
+                    foreach (Menu item in menuList)
+                    {
+                        _comboxBoxSystems.Items.Add(item.name);
+                    }
+
+                    _comboxBoxSystems.SelectedIndex = 0;
+                }
+                else
+                    MessageBox.Show("Main Menu xml cannot be found at " + menuXml);
+
+            }
+        }
+
+        #region Folder picker buttons
+        private void _buttonRoms_Click(object sender, RoutedEventArgs e)
+        {
+            Winforms.FolderBrowserDialog fb = new Winforms.FolderBrowserDialog();
+            Winforms.DialogResult result = fb.ShowDialog();
+            if (result == Winforms.DialogResult.OK)
+            {
+                _TextboxRoms.Text = fb.SelectedPath;
+            }
+        }
+        private void _buttonSnaps_Click(object sender, RoutedEventArgs e)
+        {
+            Winforms.FolderBrowserDialog fb = new Winforms.FolderBrowserDialog();
+            Winforms.DialogResult result = fb.ShowDialog();
+            if (result == Winforms.DialogResult.OK)
+            {
+                _TextboxSnaps.Text = fb.SelectedPath;
+            }
+        }
+        private void _buttonMaquee_Click(object sender, RoutedEventArgs e)
+        {
+            Winforms.FolderBrowserDialog fb = new Winforms.FolderBrowserDialog();
+            Winforms.DialogResult result = fb.ShowDialog();
+            if (result == Winforms.DialogResult.OK)
+            {
+                _TextboxMarquee.Text = fb.SelectedPath;
+            }
+        }
+        #endregion
+
+        private void _buttonModels_Click(object sender, RoutedEventArgs e)
+        {
+            Winforms.OpenFileDialog od = new Winforms.OpenFileDialog();
+            od.DefaultExt = ".mdl";
+            od.InitialDirectory = _TextboxAAPath.Text;
+            Winforms.DialogResult result = od.ShowDialog();
+
+            if (result == Winforms.DialogResult.OK)
+            {
+                //string modelPath = _TextboxModels.Text;
+                //Regex 
+                //_TextboxModels.Text = od.FileName;
+            }
+
+        }
+
+        /// <summary>
+        /// Save paths to applications config so it can be used when re launching app
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void _buttonSavePaths_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.hsPath = _TextboxHSPath.Text;
+            Properties.Settings.Default.aaPath = _TextboxAAPath.Text;
+
+            Properties.Settings.Default.Save();
+
+            _comboxBoxSystems.Items.Clear();
+            buildSystemsFromDatabase();
+            
+        }
+
+        /// <summary>
+        /// Generate items from a hyperspin database
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void _buttonGenFromHS_Click(object sender, RoutedEventArgs e)
+        {
+            string hsPath = _TextboxHSPath.Text;
+            string xml = System.IO.Path.Combine(hsPath, "Databases", _comboxBoxSystems.SelectedItem.ToString(), _comboxBoxSystems.SelectedItem.ToString() + ".xml");
+
+            if (File.Exists(xml))
+            {
+                itemGenerator.generateItemsFromHS(xml, _checkClones.IsChecked.Value, _TextboxRoms.Text, _TextboxType.Text, _TextboxRomExt.Text,
+                    _TextboxApp.Text, _TextboxModels.Text, _comboxBoxSystems.SelectedItem.ToString(), _TextboxAAPath.Text, _TextboxSnaps.Text, _TextboxMarquee.Text);
+                _canvas.IsEnabled = false;
+            }
+            else
+                MessageBox.Show("Cannot find system xml at =" + xml);
+
+            _canvas.IsEnabled = true;
+        }
+
+        private void _genRomsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                _canvas.IsEnabled = false;
+                itemGenerator.generateItemsFromFolder(_TextboxRoms.Text, _TextboxType.Text, _TextboxRomExt.Text, _TextboxApp.Text, _TextboxModels.Text, _TextboxType.Text,_TextboxAAPath.Text, _TextboxSnaps.Text, _TextboxMarquee.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed, check text fields");
+                _canvas.IsEnabled = true;
+            }
+
+            _canvas.IsEnabled = true;
+        }    
+
+
+
+    }
+}
